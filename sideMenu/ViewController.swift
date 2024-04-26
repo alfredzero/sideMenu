94a637280c8c4139c9e781e81af9f493a519b060
alfredzero <amezcuaalfredo81@gmail.com>
2024-04-26 14:43:01 -0700

    Initial Commit

---
+++ sideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sideMenu
+//
+//  Created by Alfredo Amezcua on 4/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
